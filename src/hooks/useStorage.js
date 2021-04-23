c5242698b72e4277a907f3d57556498be228fe54
thelogeshwaran <47322586+thelogeshwaran@users.noreply.github.com>
2021-04-23 07:45:55 +0530

    update storage.js

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,70 +1,58 @@
-import {
-    useEffect,
-    useState
-} from "react"
-import firebase from "firebase"
-import {
-    projectStorage,
-    projectStore
-} from "../Firebase/Config";
-import {
-    useUser
-} from "../Context/UserContext";
-import {
-    usePrivacy
-} from "../Context/PrivacyContext";
-
-
+import { useEffect, useState } from "react";
+import firebase from "firebase";
+import { projectStorage, projectStore } from "../Firebase/Config";
+import { useUser } from "../Context/UserContext";
+import { usePrivacy } from "../Context/PrivacyContext";
 
 const useStorage = (file) => {
-    const [progress, setProgress] = useState(null);
-    const [url, setUrl] = useState(null);
-    const [error, setError] = useState(null);
-    const {
-        user
-    } = useUser();
-    const {
-        privacy
-    } = usePrivacy();
-    useEffect(() => {
+  const [progress, setProgress] = useState(null);
+  const [url, setUrl] = useState(null);
+  const [error, setError] = useState(null);
+  const { user } = useUser();
+  const { privacy } = usePrivacy();
+  useEffect(() => {
+    const storageRef =
+      privacy === "personal"
+        ? projectStorage.ref(`users/${file.name}`)
+        : projectStorage.ref(`images/${file.name}`);
 
-        const storageRef = privacy === "personal" ? projectStorage.ref(`users/${file.name}`) : projectStorage.ref(`images/${file.name}`)
+    storageRef.put(file).on(
+      "state_changed",
+      (snap) => {
+        const percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+        setProgress(percentage);
+      },
+      (error) => {
+        setError(error);
+      },
+      async () => {
+        const url = await storageRef.getDownloadURL();
+        if (privacy === "public") {
+          projectStore.collection("images").add({
+            url: url,
+            likes: 0,
+            liked: false,
+            timeStamp: firebase.firestore.FieldValue.serverTimestamp(),
+          });
+          setUrl(url);
+        } else {
+          console.log("added in personal");
+          projectStore.collection("users").add({
+            url: url,
+            userId: user.uid,
+            timeStamp: firebase.firestore.FieldValue.serverTimestamp(),
+          });
+          setUrl(url);
+        }
+      }
+    );
+  }, [file, user]);
 
-        storageRef.put(file).on("state_changed", (snap) => {
-                const percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
-                setProgress(percentage);
-            },
-            (error) => {
-                setError(error)
-            },
-            async () => {
-                const url = await storageRef.getDownloadURL();
-                if (privacy === "public") {
-                   
-                    projectStore.collection("images").add({
-                        url: url,
-                        likes: 0,
-                        liked: false,
-                        timeStamp: firebase.firestore.FieldValue.serverTimestamp()
-                    })
-                    setUrl(url);
-                } else {
-                    console.log("added in personal")
-                    projectStore.collection("users").add({
-                        url: url,
-                        userId: user.uid,
-                        timeStamp: firebase.firestore.FieldValue.serverTimestamp()
-                    })
-                    setUrl(url);
-                }
-            })
-    }, [file, user])
+  return {
+    url,
+    progress,
+    error,
+  };
+};
 
-    return {
-        url,
-        progress,
-        error
-    }
-}
-
-export default useStorage
+export default useStorage;
